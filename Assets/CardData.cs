c821058bfb71ed26cda45405ff5e2c0cf9b04edd
hoangsphan <155424107+hoangsphan@users.noreply.github.com>
2025-10-25 05:00:59 +0700

    update card data

---
+++ Assets/CardData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+// using TMPro; 
+
+public class CardData : MonoBehaviour
+{
+    public enum CardType
+    {
+        Skill,
+        ManaItem,
+        RageItem
+    }
+
+    [Header("Loại thẻ")]
+    public CardType cardType = CardType.Skill;
+
+    [Header("Thông tin thẻ")]
+    public int itemValue = 0;
+    public string skillID = "Punch";
+
+    [Header("Tham chiếu (Tùy chọn)")]
+    public Image iconImage;
+    public Text labelText;
+    // public TextMeshProUGUI labelText;
+}
