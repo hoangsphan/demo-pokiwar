639170732c7a36681bc4c87a80180d0f188d2d9c
hoangsphan <155424107+hoangsphan@users.noreply.github.com>
2025-11-12 03:34:22 +0700

    add GameSession

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+// Script này dùng để vận chuyển data (như chọn enemy)
+// qua các scene mà không cần các Manager phức tạp phải biết nhau.
+public class GameSession : MonoBehaviour
+{
+    public static GameSession Instance { get; private set; }
+
+    // Dữ liệu cần vận chuyển
+    public GameObject selectedEnemyPrefab;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            // Gắn vào DontDestroyOnLoad (vì nó sẽ nằm trên DatabaseManager)
+        }
+        else
+        {
+            // Không tự hủy, vì nó nằm trên object đã DontDestroyOnLoad
+        }
+    }
+}
