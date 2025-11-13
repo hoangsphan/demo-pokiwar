3ef1c88c24c16e3447c5aa5f2addf580ef60e38e
hoangsphan <155424107+hoangsphan@users.noreply.github.com>
2025-11-14 01:19:53 +0700

    update game session

---
+++ GameSession.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic; // <-- THÊM DÒNG NÀY
+
+public class GameSession : MonoBehaviour
+{
+    public static GameSession Instance { get; private set; }
+
+    // Dữ liệu cần vận chuyển
+    public GameObject selectedEnemyPrefab;
+    public string previousSceneName; // Dùng để lưu tên map (ví dụ: "FireMap")
+
+    // === THÊM CÁC BIẾN NÀY ĐỂ MANG DATA CHO BATTLEMANAGER ===
+    public GameObject selectedPetPrefab;
+    public List<GameObject> selectedCardPrefabs = new List<GameObject>();
+    // =======================================================
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            // Script này nằm trên DatabaseManager, 
+            // mà DatabaseManager đã DontDestroyOnLoad, nên nó sẽ sống.
+        }
+        else
+        {
+            // Không tự hủy, vì nó nằm trên object đã DontDestroyOnLoad
+        }
+    }
+}
